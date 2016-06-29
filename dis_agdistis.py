a6e5d0a78f68fc4ae9922c2ffd8e495480d6bbe8
filipdbrsk <filip.dbrsk@gmail.com>
2016-06-30 00:00:32 +0200

    Compute accuracy for agdistis

--- dis_agdistis.py
+++ dis_agdistis.py
@@ -5,14 +5,15 @@
 
 from urllib.parse import urlencode
 from urllib.request import urlopen, Request
+import json
 
 disambiguatorUrl="http://139.18.2.164:8080/AGDISTIS"
 
 def disambiguate(xmlText, type='agdistis'):
 	params={"text": xmlText, "type": type}
 	request = Request(disambiguatorUrl, urlencode(params).encode())
-	json = urlopen(request).read().decode()
-	print(json)
+	thisJson = urlopen(request).read().decode()
+	return json.loads(thisJson)
 
 if __name__ == '__main__':
 
--- run_meantime.py
+++ run_meantime.py
@@ -5,20 +5,26 @@ import sys
 
 if __name__=="__main__":
 	corpus=sys.argv[1]
+	same=0
+	notSame=0
 	for file in os.listdir(corpus):
 		print(file)
 		filename=corpus.strip('/') + '/' + file
 		myXml, entities=utils.naf2inlineEntities(filename)
 		print(myXml)
 		print(entities)
-		dis_agdistis.disambiguate(myXml, "agdistis")
+		da=dis_agdistis.disambiguate(myXml, "agdistis")
 		for agd_entity in da:
+			print(agd_entity)
 			offset=str(agd_entity["start"])
-			agd_link=agd_entity["disambiguatedURL"]
+			agd_link=str(agd_entity["disambiguatedURL"])
+			goldlink=str(entities[offset])
+			print(agd_link, goldlink)
 			if agd_link==goldlink:
-				print("same")
+				same+=1
 			else:
-				print("not same")
+				notSame+=1
 			
 
 		break
+	print("Same: %d. Not same: %d. Accuracy: %f" % (same, notSame, same/(same+notSame)))
